a5c38d7761afed3d32ecd481950527aae08f74b0
eyasuh <eyha12@gmail.com>
2020-04-15 12:17:07 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Create App: Identifying the variables
---
+++ src/MyTrip.java
@@ -0,0 +1,17 @@
+public class MyTrip {
+    public static void main(String[] args) {
+        //declare variables
+        String country = "Kenya";
+        double population = 49.7;
+        String capital = "Nairobi";
+        double currency = 103.84;
+        String currencyName = "Kenyan Shillings!!";
+        //print out letter using variables
+        System.out.println("Hey Mom and Dad! \n" +
+                "Here are some intersting details about the first country I'm visiting.\nName: " + country +
+                "\nPopulation Size: " + population + " million" +
+                "\nCapital: " + capital +
+                "\nCurrency: One U.S Dollar equals $" + currency + " " + currencyName +
+                "\nI'm so excited to visit the Nairobi Nation Park and feel the giraffes!! Talk later");
+    }
+}
